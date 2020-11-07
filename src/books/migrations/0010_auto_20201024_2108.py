909e53056f8ad67a89e2bf615dae1c7bdbd05385
zmiterpimenau <71035667+zmiterpimenau@users.noreply.github.com>
2020-11-07 16:35:25 +0300

    Refactoring2

--- src/books/migrations/0010_auto_20201024_2108.py
+++ src/books/migrations/0010_auto_20201024_2108.py
@@ -3,7 +3,7 @@
 from django.db import migrations, models
 import django.utils.timezone
 
-
+ 
 class Migration(migrations.Migration):
 
     dependencies = [
--- src/books/migrations/0023_auto_20201105_2358.py
+++ src/books/migrations/0023_auto_20201105_2358.py
@@ -2,7 +2,7 @@
 
 from django.db import migrations, models
 import django.db.models.deletion
-
+ 
 
 
 class Migration(migrations.Migration):
